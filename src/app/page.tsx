9209c689ab227c80fa666f22783aaf9a3a50c2bf
Your Name <you@example.com>
2024-06-20 00:14:55 +0530

    'ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 import Image from "next/image";
 import { PanelsTopLeft } from "lucide-react";
@@ -5,6 +6,8 @@ import { ArrowRightIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { SignedIn } from "@clerk/nextjs";
+import { SignInButton, SignedOut, UserProfile } from "@clerk/clerk-react";
 
 export default function HomePage() {
   return (
@@ -16,8 +19,8 @@ export default function HomePage() {
             className="flex justify-start items-center hover:opacity-85 transition-opacity duration-300"
           >
             <PanelsTopLeft className="w-6 h-6 mr-3" />
-            <span className="font-bold">shadcn/ui sidebar</span>
-            <span className="sr-only">shadcn/ui sidebar</span>
+            <span className="font-bold">Hop Shop</span>
+            <span className="sr-only">Hop Shop</span>
           </Link>
           <nav className="ml-auto flex items-center gap-2">
             <Button
@@ -26,11 +29,18 @@ export default function HomePage() {
               className="rounded-full w-8 h-8 bg-background"
               asChild
             >
-              <Link href="https://github.com/salimi-my/shadcn-ui-sidebar">
+              {/* <Link href="https://github.com/salimi-my/shadcn-ui-sidebar">
                 <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
-              </Link>
+              </Link> */}
             </Button>
             <ModeToggle />
+            <SignedIn>
+              <UserProfile></UserProfile>
+            </SignedIn>
+
+              <SignedOut>
+                <SignInButton></SignInButton>
+              </SignedOut>
           </nav>
         </div>
       </header>
@@ -38,16 +48,15 @@ export default function HomePage() {
         <div className="container relative pb-10">
           <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
             <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
-              Sidebar example built on top of shadcn/ui
+            Hop Shop - Your One-Stop Marketplace
             </h1>
             <span className="max-w-[750px] text-center text-lg font-light text-foreground">
-              A stunning and functional retractable sidebar for Next.js using
-              shadcn/ui complete with desktop and mobile responsiveness.
+            Discover a seamless shopping experience with Hop Shop, offering everything from groceries and medicine to fashion and food, all in one convenient place.
             </span>
             <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
               <Button variant="default" asChild>
                 <Link href="/dashboard">
-                  Demo
+                  Get Started
                   <ArrowRightIcon className="ml-2" />
                 </Link>
               </Button>
@@ -57,14 +66,14 @@ export default function HomePage() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Learn shadcn/ui
+                  Shop Now
                 </Link>
               </Button>
             </div>
           </section>
           <div className="w-full flex justify-center relative">
             <Image
-              src="/demo-light-min.png"
+              src="/ecomm.png"
               width={1080}
               height={608}
               alt="demo"
@@ -72,52 +81,52 @@ export default function HomePage() {
               className="border rounded-xl shadow-sm dark:hidden"
             />
             <Image
-              src="/demo-dark-min.png"
+              src="/ecomm.png"
               width={1080}
               height={608}
               alt="demo-dark"
               priority
               className="border border-zinc-600 rounded-xl shadow-sm hidden dark:block dark:shadow-gray-500/5"
             />
-            <Image
+            {/* <Image
               src="/demo-mobile-light-min.png"
               width={228}
               height={494}
               alt="demo-mobile"
               className="border rounded-xl absolute bottom-0 right-0 hidden lg:block dark:hidden"
-            />
-            <Image
+            /> */}
+            {/* <Image
               src="/demo-mobile-dark-min.png"
               width={228}
               height={494}
               alt="demo-mobile"
               className="border border-zinc-600 rounded-xl absolute bottom-0 right-0 hidden dark:lg:block"
-            />
+            /> */}
           </div>
         </div>
       </main>
       <footer className="py-6 md:py-0 border-t border-border/40">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
           <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
-            Built on top of{" "}
+            Built with love by - 
             <Link
               href="https://ui.shadcn.com"
               target="_blank"
               rel="noopener noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              shadcn/ui
+               HopShop.
             </Link>
-            . The source code is available on{" "}
+            
             <Link
               href="https://github.com/salimi-my/shadcn-ui-sidebar"
               target="_blank"
               rel="noopener noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              GitHub
+        
             </Link>
-            .
+            
           </p>
         </div>
       </footer>
